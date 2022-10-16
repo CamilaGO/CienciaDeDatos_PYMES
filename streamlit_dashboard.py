45894feb3dab5e8324b867b2469ea6920dd27b59
Camila <gon18398@uvg.edu.gt>
2022-10-15 20:05:36 -0600

    se muestra solo 2 veces el df en limpieza

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -66,8 +66,7 @@ if selected == "Limpieza":
       for x in df.columns:
         if df[x].dtype == 'object':
           df[x] = df[x].str.lower()
-          df[x] = df[x].str.replace('\W', '', regex=True) #se elimina todos los caracteres no alfabeticos
-          
+
       # Como se ha observado todos los nombres tienen una secuencia de ';;;;;;;' al final que posiblemente fue un error al momento de 
       # guardar los datos. Procederemos a eliminar dichos caracteres y letras con tildes, el simbolo de dolar y la ñ
       df.replace(regex={';': '', 
